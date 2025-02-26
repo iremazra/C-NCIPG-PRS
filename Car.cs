020296c30c95a39c71a7829df44d0de834b701c4
iremazra <iremazra26@gmail.com>
2025-02-26 19:49:44 +0300

    İlk commit

---
+++ Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public string Brand { get; set; }
+      
+        public void brandOfCar()
+        {
+            Console.WriteLine("This car belongs to " + this.Brand + " brand.");
+        }
+
+        public void modelOfCar()
+        {
+            Console.WriteLine("This car's model is " + this.Model+".");
+        }
+
+        
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Cars;
+
+Console.WriteLine("Hello, World!");
+
+Audi audi=new Audi();
+Mercedes mercedes=new Mercedes();
+Toyoto toyoto=new Toyoto();
+Togg togg=new Togg();
+
+audi.Brand = "Audi";
+audi.Model = "A6 Avant e-tron";
+
+mercedes.Brand = "Mercedes";
+mercedes.Model = "Mercedes-Benz GLC";
+
+togg.Brand = "TOGG";
+togg.Model = "T10X";
+
+toyoto.Brand= "Toyoto";
+toyoto.Model = "Corolla";
+
+audi.brandOfCar();
+audi.modelOfCar();
+Console.WriteLine();
+
+mercedes.brandOfCar();
+mercedes.modelOfCar();
+Console.WriteLine();
+
+toyoto.brandOfCar();
+toyoto.modelOfCar();
+Console.WriteLine();
+
+togg.brandOfCar();
+togg.modelOfCar();
+Console.WriteLine();
